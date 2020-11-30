680a395f36fb4b74ede94082d0f38e6b51d75425
William Truppi <williamt@hotmail.it>
2020-11-30 19:07:04 +0100

    correzione al codice css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,12 @@
 var links = $("li.menu");
 console.log(links);
-var dropdownMenu = $(".navbar_center li > ul").siblings().get();
-console.log(dropdownMenu);
 
 links.mouseenter(function () { 
   $('ul', this).addClass("visible");
+
 });
 
-$(document).click(function () { 
+links.mouseleave(function () { 
   if ($("ul", this).hasClass("visible")){
     $('ul', this).removeClass("visible");
   }
